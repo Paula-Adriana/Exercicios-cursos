028df664fd937905c7d0586793f3f4586470319d
safirablock <85121210+safirablock@users.noreply.github.com>
2021-06-05 17:33:15 -0300

    IFs aninhados

---
+++ IGTI/JAVA/praticaIfelseIf/src/praticaIfelseIf/praticaIfelseIf.java
@@ -0,0 +1,47 @@
+package praticaIfelseIf;
+
+import java.util.Scanner;
+
+public class praticaIfelseIf {
+
+	public static void main(String[] args) {
+		
+		
+		
+		double n1, n2, n3, total, mediaAvaliacoes, freq;
+		String situacao;
+		Scanner entrada = new Scanner(System.in); //recebe dados do teclado
+		
+		//Entada de notas
+		System.out.println("Digite a primeira nota: ");
+		n1 = entrada.nextDouble();
+		System.out.println("Digite a segunda nota: ");
+		n2 = entrada.nextDouble();
+		System.out.println("Digite a terceira nota: ");
+		n3 = entrada.nextDouble();
+		
+		total = n1+n2+n3;
+		mediaAvaliacoes = total/3;
+		
+		System.out.println("Digite a frequência do aluno: ");
+		freq = entrada.nextDouble();
+		
+		if (total >= 70 && freq >= 0.75) {
+			if (total >= 90)
+			situacao = "Aprovado com conceito A";
+			else if (total >=80 && total < 90)
+				situacao = "Aprovado com conceito B";
+			else 
+				situacao = "Aprovado com conceito C";	
+		}
+		else if (total >= 40 && total < 70 && freq >= 0.75)
+			situacao = "Exame especial";
+		else
+			situacao = "Reprovado";
+		
+		System.out.printf("O aluno obteve nota total %.2f e média por avalição de %.2f. A frequência foi %.2f%%. Com isso o aluno "
+				+ "está %s.", total, mediaAvaliacoes, freq*100, situacao);
+	}
+	}
+
+
