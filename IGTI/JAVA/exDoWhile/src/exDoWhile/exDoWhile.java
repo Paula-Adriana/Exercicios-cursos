e680127e2c990620652d748a5567feca8ad63dc8
safirablock <85121210+safirablock@users.noreply.github.com>
2021-06-06 10:53:14 -0300

    ex DoWhile

---
+++ IGTI/JAVA/exDoWhile/src/exDoWhile/exDoWhile.java
@@ -0,0 +1,26 @@
+package exDoWhile;
+
+import java.util.*;
+
+public class exDoWhile {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		char continuar = ' ';
+		System.out.println("Deseja Continuar (S - Sim; N - Não)?");
+		continuar = entrada.nextLine().charAt(0);
+		
+	/*	while (continuar == 'S') {
+			System.out.println("Mensagem: ");
+			System.out.println("Deseja Continuar (s - sim; n - nï¿½o)?");
+			continuar = entrada.nextLine().charAt(0);
+
+		}
+	*/	do {
+			System.out.println("Mensagem: ");
+			System.out.println("Deseja Continuar (S - Sim; N - Não)?");
+			continuar = entrada.nextLine().charAt(0);
+		} while (continuar == 'S');
+
+}
+}
