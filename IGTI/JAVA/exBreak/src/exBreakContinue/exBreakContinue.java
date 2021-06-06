14cdc5864ddd019a14ef4b1ce5648524f9cb990f
safirablock <85121210+safirablock@users.noreply.github.com>
2021-06-06 12:52:15 -0300

    break ok

--- IGTI/JAVA/exBreak/src/exBreakContinue/exBreakContinue.java
+++ IGTI/JAVA/exBreak/src/exBreakContinue/exBreakContinue.java
@@ -17,6 +17,9 @@ public class exBreakContinue {
 			
 			System.out.println("Digite o número desejado: ");
 			numero = entrada.nextInt();
+			
+			if (numero == 10) //caso o usuario digite determinado valor (ex: 10) com o break é forçado a parar
+				break;
 		}
 			System.out.printf("O somatório foi %d.", somatorio);
 	}
