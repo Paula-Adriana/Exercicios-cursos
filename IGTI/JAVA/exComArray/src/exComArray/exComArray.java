ecaa8b7555d4834eb1a9cc35cc01803743ecef37
safirablock <85121210+safirablock@users.noreply.github.com>
2021-06-06 12:37:33 -0300

    updata aula terminada array

--- IGTI/JAVA/exComArray/src/exComArray/exComArray.java
+++ IGTI/JAVA/exComArray/src/exComArray/exComArray.java
@@ -8,26 +8,17 @@ public class exComArray {
 		//int [] nome = new tipo [tamanho]
 		
 		int [] numeros = new int [5];
-		int total = 0;
+		int total = 0, i = 0;
 		Scanner ent = new Scanner(System.in);
 		
-		System.out.println("Digite o primeiro número: ");
-		numeros[0] = ent.nextInt();
+		for (i = 0; i < 5; i++) {
+			System.out.printf("Digite o %dº número: ", i+1);//i+1 para que nao apareca: digite o 0º numero
+			numeros[i ] = ent.nextInt();
+			total = total + numeros[i];
+			System.out.printf("Total parcial: %d.\n", total);
+		}
 		
-		System.out.println("Digite o segundo número: ");
-		numeros[1] = ent.nextInt();
 		
-		System.out.println("Digite o terceiro número: ");
-		numeros[2] = ent.nextInt();
-		
-		System.out.println("Digite o quarto número: ");
-		numeros[3] = ent.nextInt();
-
-		System.out.println("Digite o quinto número: ");
-		numeros[4] = ent.nextInt();
-
-		
-		total = numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4];
 		System.out.printf("O valor total é %d.\n", total);
 				
 
