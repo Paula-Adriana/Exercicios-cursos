1fc689263abafdd156315aa1d02bb21e1c72efe4
safirablock <85121210+safirablock@users.noreply.github.com>
2021-06-06 09:13:21 -0300

    Calculadora com For

---
+++ IGTI/JAVA/tabuadaComFor/src/Ex1comFor/Ex1comFor.java
@@ -0,0 +1,17 @@
+package Ex1comFor;
+
+public class Ex1comFor {
+
+	public static void main(String[] args) {
+	
+		int i = 0, j = 0;
+		
+		for (i = 0; i <10; i++) {
+			System.out.printf("Tabuada do %d\n", i);
+			for (j = 0; j < 10; j++) {
+				System.out.printf("%d * %d = %d\n", i, j, i*j);
+				}
+
+		}
+	}
+	}
