37228bcb46fdf244da38f8bf4dc30dcb4b3df6b2
safirablock <85121210+safirablock@users.noreply.github.com>
2021-06-18 16:12:31 -0300

    Primeiros passos em python

---
+++ Ex_Integer.py
@@ -0,0 +1,17 @@
+# Declarando
+numero_aluno = int(50)
+print(numero_aluno)
+
+#Se nao for declarado como acima, a divisao de um integer/integer será float. 
+divisao = 50/2
+print(divisao) # 25.0
+print(type(divisao)) # <class 'float'>
+
+divisao = int(50/2)
+print(divisao) # 25
+print(type(divisao)) # <class 'int'>
+
+a = 60
+print(a)
+print(type(a)) #irá retornar: <class 'int'> ou seja, do tipo inteiro. 
+
---
+++ Ex_String.py
@@ -0,0 +1,18 @@
+rua = 'Avenida Marcelino de Almeida Fontoura, '
+numero = '150, '
+bairro = 'Sul, '
+cidade = 'Rio de Janeiro, '
+estado = 'RJ, '
+cep = '22220-000'
+
+#Concatenando strings
+endereco = rua + numero + bairro + cidade + estado + cep
+print(endereco)
+
+#Para maiúsculas
+endereco_maiscula = endereco.upper()
+print(endereco_maiscula)
+
+#Para minúsculas
+endereco_minúsculas = endereco.lower()
+print(endereco_minúsculas)
