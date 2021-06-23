2b2c96f33678d21d7417d89f30c8ae041e0cab08
safirablock <85121210+safirablock@users.noreply.github.com>
2021-06-23 15:11:37 -0300

    modulos

---
+++ IGTI/PYTHON/modulos.py
@@ -0,0 +1,12 @@
+import math
+raiz = math.sqrt(9)
+print(raiz)
+
+# Se for um módulo seu será:
+#import seu_modulo.py
+
+# Para importar parte de um módulo
+# from math import factorial
+
+# -------------------------
+# Verificar a documentação do módulo utilizado para saber quais funções existem, argumentos necessários etc 
